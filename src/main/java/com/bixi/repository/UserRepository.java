76981592548c4f32b4b5df12b19e54c274236cc8
Ananda Babu <anand.babu222@gmail.com>
2019-01-20 01:12:51 -0500

    Added signup

--- src/main/java/com/bixi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bixi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bixi.data.domain.model.db.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findOneByUsername(String username);
-}
